a8d8494a8936ab5b17e2c77d31eadb75aaa50e09
Maanik Singh <mas523@lehigh.edu>
2019-11-13 00:05:07 -0500

    fixed r-script

--- Assignment 05.R
+++ Assignment 05.R
@@ -1,14 +1,14 @@
-# Clean Up 
 rm(list=ls(all=TRUE))
 cat("\014")
 
-# *.csv 
-file.edit(".gitignore")
+if (!require("data.table")) install.packages("data.table")
+library("data.table")
 
 header <- read.table("CPIAUCSL.csv", header = TRUE,
                      sep=",", nrow = 1)
-DF <- fread("CPIAUCSL.csv", skip=1, sep=",",
-            header=FALSE, data.table=FALSE)
+DF <- fread("CPIAUCSL.csv",
+                                 skip=1, sep=",",header=FALSE,
+                                 data.table=FALSE)
 setnames(DF, colnames(header))
 rm(header)
 
